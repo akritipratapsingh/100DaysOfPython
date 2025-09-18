12aba7f299501af8f3d77433918919ca19767ab2
Akriti Pratap Singh <143482301+akritipratapsingh@users.noreply.github.com>
2025-09-18 14:39:15 +0530

    Update PrimeNumberDetector.py

--- Day02/PrimrNumberDetector.py
+++ Day02/PrimrNumberDetector.py
@@ -1,13 +1,17 @@
 def PrimeNumberDetector(num):
     numSqrt = int(num**0.5)
+    
     if num <= 1:
         print('Not Prime')
+    elif num == 2:
+        print('Prime')
     
     for i in range(2,numSqrt+1):
         if num%i == 0:
             a = "Not Prime"
             print(a)
             break
+        
         
     
     
